8d223d6b2d9bc77cad8c508287fce09cc67b8777
Noveen Sachdeva <noveen.sachdeva@research.iiit.ac.in>
2016-12-15 23:43:29 +0530

    changed.

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ var app = express();
 app.use(express.static('view'));
 
 app.get('/', function(request, response) {
-	response.redirect("home.html")
+	response.redirect("login.html")
+});
+
+app.get('/check', function(request, response) {
+	console.log("request aa gayi!");
 });
 
 var server = app.listen(3000, function() {
