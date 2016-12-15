5f06d943108fd1708951bcfbf15393f8e583af26
Noveen Sachdeva <noveen.sachdeva@research.iiit.ac.in>
2016-12-16 00:29:09 +0530

    database added.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
-
+var fs = require("fs");
 var app = express();
-
 app.use(express.static('view'));
 
 app.get('/', function(request, response) {
@@ -10,6 +9,52 @@ app.get('/', function(request, response) {
 
 app.get('/check', function(request, response) {
 	console.log("request aa gayi!");
+
+	fs.readFile('users.csv', function(err, data) {
+		if(err) {
+			response.send("error!");
+		}
+
+		var users = data.toString().split('\n');
+		var n = users.length;
+		var uname = request.query.uname;
+		var pass = request.query.pass;
+
+		console.log(uname, pass);
+
+		for(var i=0;i<n;i++) {
+			var det = users[i].split(',');
+
+			if(det[0] == uname) {
+				if(det[1] == pass) {
+					response.send("2");
+				}
+				else {
+					response.send("1");
+				}
+			}
+
+			else {
+				response.send("0");
+			}
+		}
+	})
+});
+
+app.get('/newUser', function(request, response) {
+
+	var uname = request.query.uname;
+	var pass = request.query.pass;
+
+	console.log(uname, pass);
+
+	fs.appendFile('users.csv', uname+','+pass+'\n', function(err) {
+		if(err) {
+			response.end('0');
+		}
+	});
+
+	response.end('1');
 });
 
 var server = app.listen(3000, function() {
