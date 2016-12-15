43bb909b4771b2ff64b359907fa7d562f4b45b9b
amanshahi <amanshahi2016@gmail.com>
2016-12-16 01:47:19 +0530

    login.js file was updated

--- view/login.js
+++ view/login.js
@@ -1,18 +1,16 @@
 var Logmain=angular.module('ProjectApp',[]);
 
 Logmain.controller('ProjectController', function($scope, $http){
-	console.log($scope.User_name);
 	var uname, pass;
 	$scope.send_data = function(username, password){
 		uname = username;
 		pass = password;
-		alert($scope.uname);
+		var link = 'localhost:3000/check?uname='+uname+'&pass='+pass;
+		alert(link);
+		$http.get(link)
+		.success(function(response){
+			alert(response);
+			$scope.var = response;
+		});
 	};
-	var user = $scope.uname;
-	var pass = $scope.pass;
-	var link = 'localhost:3000/check?uname='+user+'&pass='+pass;
-	$http.get(link)
-	.success(function(response){
-		$scope.var = response;
-	});
 });
