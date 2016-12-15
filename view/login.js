831ceda318004dbb757ce9e3a4d7e1dc4787466d
Noveen Sachdeva <noveen.sachdeva@research.iiit.ac.in>
2016-12-16 01:55:29 +0530

    login.

--- view/login.js
+++ view/login.js
@@ -5,11 +5,11 @@ Logmain.controller('ProjectController', function($scope, $http){
 	$scope.send_data = function(username, password){
 		uname = username;
 		pass = password;
-		var link = 'localhost:3000/check?uname='+uname+'&pass='+pass;
-		alert(link);
+		var link = 'check?uname='+uname+'&pass='+pass;
+		//alert(link);
 		$http.get(link)
 		.success(function(response){
-			alert(response);
+			alert(response.data);
 			$scope.var = response;
 		});
 	};
