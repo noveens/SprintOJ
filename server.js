c308dffc057bfd62112e6f5117740139401b4880
Noveen Sachdeva <noveen.sachdeva@research.iiit.ac.in>
2016-12-17 21:04:15 +0530

    score APIs added.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var fileUpload = require('express-fileupload');
 var exec = require("child_process").exec;
 var sys = require("sys");
 var url = require("url");
+var replace = require("replace");
 var app = express();
 
 app.use(express.static('view'));
@@ -33,7 +34,7 @@ app.get('/check', function(request, response) {
 			var det = users[i].split(',');
 
 			if(det[0] == uname) {
-				if( passwordHash.verify(pass, det[1]) == true ) {
+				if( passwordHash.verify(pass, det[2]) == true ) {
 					response.end("2");
 				}
 				else {
@@ -78,7 +79,7 @@ app.get('/newUser', function(request, response) {
 		}
 
 		if(fl == 0 && uname != 'undefined') {
-			fs.appendFile('users.csv', uname+','+hashedPass+'\n', function(err) {
+			fs.appendFile('users.csv', uname+','+'0'+','+hashedPass+'\n', function(err) {
 				if(err) {
 					response.end('0');
 				}
@@ -145,6 +146,104 @@ app.get("/getTestcase", function(request, response) {
 	temp.on("end", function() {
 		response.end();
 	})
+});
+
+app.get("/getScore", function(request, response) {
+	var name = request.query.name;
+
+	fs.readFile('users.csv', function(err, data) {
+		if(err) {
+			response.send("error!");
+		}
+
+		var users = data.toString().split('\n');
+		var n = users.length;
+
+		if(name == 'undefined') {
+			response.end(-1);
+		}
+
+		var fl = 0;
+
+		for(var i=0;i<n;i++) {
+
+			var det = users[i].split(',');
+			if(det[0] == name) {
+				fl = 1;
+				response.end(det[1]);
+				break;
+			}
+		}
+
+		if(fl == 0) {
+			response.end(-1);
+		}
+	})
+});
+
+app.get('/addScore', function(request, response) {
+	var name = request.query.name;
+	var add = request.query.add;
+
+	/*replace({
+	  regex: "foo",
+	  replacement: "bar",
+	  paths: ['.'],
+	  recursive: true,
+	  silent: true,
+	});*/
+
+	fs.readFile('users.csv', function(err, data) {
+		if(err) {
+			response.send("error!");
+		}
+
+		var users = data.toString().split('\n');
+		var n = users.length;
+		var fl = 0;
+
+		if(name == 'undefined') {
+			response.end('0');
+		}
+
+		for(var i=0;i<n;i++) {
+			var det = users[i].split(',');
+
+			if(det[0] == name) {
+				var temp = Number(det[1]) + Number(add);
+				var rep = det[0] + "," + det[1];
+				var rep2 = det[0]+","+temp.toString();
+				
+				replace({
+				  regex: rep,
+				  replacement: rep2,
+				  paths: ['./users.csv'],
+				  recursive: false,
+				  silent: true,
+				});
+
+				console.log("score of " + name + " changed by " + add + ", new score = " + temp);
+
+				response.end('1');
+				fl = 1;
+				break;
+			}
+		}
+
+		if(fl == 0) {
+			response.end('0');
+		}
+
+		/*if(fl == 1 && uname != 'undefined') {
+			fs.appendFile('users.csv', uname+','+hashedPass+','+'0'+'\n', function(err) {
+				if(err) {
+					response.end('0');
+				}
+			});
+			response.end('1');
+		}*/
+	})
+
 });
 
 var server = app.listen(3000, function() {
--- temp/code.cpp
+++ temp/code.cpp
@@ -1,29 +1,122 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int cmp(void *a, void *b)
+typedef struct node
 {
-	return *(int *)a - *(int *)b;
+	int val;
+	struct node *left;
+	struct node *right;
+}node;
+
+node *root=NULL;
+
+int in[10004];
+
+int insert(node *start, int n)
+{
+	if(root==NULL)
+	{
+		node *temp=(node *)malloc(sizeof(node));
+		temp->val=n;
+		temp->left=NULL;
+		temp->right=NULL;
+		root=temp;
+	}
+	else
+	{
+		if(n >= start->val)
+		{
+			if(start->right == NULL)
+			{
+				// insert node here
+				node *temp=(node *)malloc(sizeof(node));
+				temp->val=n;
+				temp->left=NULL;
+				temp->right=NULL;
+				start->right=temp;
+			}
+			else
+				insert(start->right, n);
+		}
+		else
+		{
+			if(start->left == NULL)
+			{
+				//insert node here
+				node *temp=(node *)malloc(sizeof(node));
+				temp->val=n;
+				temp->left=NULL;
+				temp->right=NULL;
+				start->left=temp;
+			}
+			else
+				insert(start->left, n);
+		}
+	}
+	return(0);
+}
+
+int find(int n, node *start)
+{
+	if(start==NULL)
+	{
+		return(0);
+	}
+
+	if(n > start->val)
+	{
+		find(n, start->right);
+	}
+	else if(n < start->val)
+	{
+		find(n, start->left);
+	}
+	else
+	{
+		int i,sum=0;
+		for(i=0;i<n;i++)
+		{
+			if(in[i]>=n)
+				sum+=in[i];
+		}
+		return sum;
+	}
+}
+
+int s=0;
+
+int infix(node *start)
+{
+	if(start==NULL)
+		return(0);
+	infix(start->left);
+	in[s]=start->val;
+	s++;
+	infix(start->right);
+	return(0);
 }
 
 int main(void)
 {
-	int n;
+	int n,i,temp,j;
 	scanf("%d", &n);
-	int arr[n],i,cum[n];
 	for(i=0;i<n;i++)
-		scanf("%d", &arr[i]);
-	qsort(arr,n,sizeof(int),cmp);
-	for(i=n-1;i>=0;i--)
 	{
-		if(i==n-1)
-			cum[i]=arr[i];
-		else
-		{
-			cum[i]=cum[i+1]+arr[i];
-		}
+		scanf("%d", &temp);
+		insert(root,temp);
 	}
 	for(i=0;i<n;i++)
-		printf("%d ", cum[i]);
-	printf("\n");
+		printf("%d ", in[i]);
+	for(i=0;i<n;i++)
+	{
+		//int ans;
+		//ans=find(in[i], root);
+		//printf("%d\n", ans);
+		int ans;
+		for(j=i;j<n;j++)
+			ans+=in[i];
+		printf("%d\n", ans);
+	}
+
 	return(0);
 }
