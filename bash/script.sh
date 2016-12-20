e5a4740f57ecea686bc7a99d4f8e4cbe503e0485
amanmehta-maniac <amanmehta1997@gmail.com>
2016-12-20 16:10:10 +0530

    LOC reduced and .c added

--- bash/script.sh
+++ bash/script.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 #echo "into .cpp"
-if [ $3 == ".cpp" ]; then
+a="./testcases/"
+b=$a$1
+c=$b"/"
+#./testcases/1/
+n=3
+#for ((i=1; i<=$n; i++))
+#do
+d=$c"in_"
+e=$d$2
+f=$c"out_"
+g=$f$2
+h="temp"$2
+if [ $3 == ".cpp" || $3 == ".c" ]; then
+	if [ $3 == ".cpp" ]; then
 	g++ ./temp/code.cpp -std=c++11 -o $2
-	a="./testcases/"
-	b=$a$1
-	c=$b"/"
-	#./testcases/1/
-	n=3
-	#for ((i=1; i<=$n; i++))
-	#do
-		d=$c"in_"
-		e=$d$2
-		f=$c"out_"
-		g=$f$2
-		h="temp"$2
-		./$2 < $e > $h
-		echo $2
-		diff $h $g | wc -l 
-	#done
+	fi
+
+	if [ $3 == ".c" ]; then
+	gcc ./temp/code.c -o $2
+	fi
+
+	./$2 < $e > $h
+	echo $2
+	diff $h $g | wc -l 
 	rm $2 $h
 fi
+
 if [ $3 == ".py" ]; then
-	a="./testcases/"
-	b=$a$1
-	c=$b"/"
-	#./testcases/1/
-	n=3
-	#for ((i=1; i<=$n; i++))
-	#do
-		d=$c"in_"
-		e=$d$2
-		f=$c"out_"
-		g=$f$2
-		h="temp"$2
-		python ./temp/code.py < $e > $h
-		echo $2
-		diff $h $g | wc -l
+	python ./temp/code.py < $e > $h
+	echo $2
+	diff $h $g | wc -l
 	rm $2 $h
 fi
+
