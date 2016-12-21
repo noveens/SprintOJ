b75846de7885b5e13277e7d9a35924de6778309c
amanmehta-maniac <amanmehta1997@gmail.com>
2016-12-21 21:28:26 +0530

    script for java program added

--- bash/script.sh
+++ bash/script.sh
@@ -57,3 +57,21 @@ if [ $3 == ".py" ]; then
 		diff $h $g | wc -l
 	rm $2 $h
 fi
+if [ $3 == ".py" ]; then
+	a="./testcases/"
+	b=$a$1
+	c=$b"/"
+	#./testcases/1/
+	n=3
+	#for ((i=1; i<=$n; i++))
+	#do
+		d=$c"in_"
+		e=$d$2
+		f=$c"out_"
+		g=$f$2
+		h="temp"$2
+		java ./temp/code.java < $e > $h
+		echo $2
+		diff $h $g | wc -l
+	rm $2 $h
+fi
